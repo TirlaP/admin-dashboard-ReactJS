72dc9bc976e035028f00ea19db3880aa2e980ced
Petru Tirla <petru.tirla@student.usv.ro>
2022-07-28 10:20:30 +0300

    Admin-Dashboard-ReactJS

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const API_URL = "https://randomuser.me/api/";
+
+const Employees = () => {
+  const searchEmployees = async (title) => {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  // searchMovies();
+
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const API_URL = "https://randomuser.me/api/";
+
+const Orders = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  // searchMovies();
+
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Orders;
